afe429bead822100eb11c37f812695b38b9bd792
AbdElrahman Yehia <abdoo.ay@gmail.com>
2026-05-11 14:08:57 +0300

    Improve elbow arrow dragging

--- src/tldraw/config.ts
+++ src/tldraw/config.ts
@@ -1,10 +1,19 @@
-import { defaultBindingUtils, defaultShapeUtils } from 'tldraw'
+import { ArrowShapeUtil, defaultBindingUtils, defaultShapeUtils } from 'tldraw'
 import { DatabaseShapeUtil } from './shapes/DatabaseShape'
 import { SvgSymbolShapeUtil } from './shapes/SvgSymbolShape'
 import { DatabaseTool } from './tools/DatabaseTool'
 import { SvgSymbolTool } from './tools/SvgSymbolTool'
 
-export const shapeUtils = [DatabaseShapeUtil, SvgSymbolShapeUtil] as const
-export const syncShapeUtils = [...defaultShapeUtils, ...shapeUtils] as const
+const SketchBoardArrowShapeUtil = ArrowShapeUtil.configure({
+	elbowMidpointSnapDistance: 0,
+	elbowMinSegmentLengthToShowMidpointHandle: 8,
+	minElbowHandleDistance: 28,
+})
+
+export const shapeUtils = [SketchBoardArrowShapeUtil, DatabaseShapeUtil, SvgSymbolShapeUtil] as const
+export const syncShapeUtils = [
+	...defaultShapeUtils.filter((shapeUtil) => shapeUtil.type !== 'arrow'),
+	...shapeUtils,
+] as const
 export const syncBindingUtils = defaultBindingUtils
 export const tools = [DatabaseTool, SvgSymbolTool] as const
